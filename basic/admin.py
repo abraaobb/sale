85785c60160be07376a912d6431348eb9e537acc
Abraao Brandao <abraao.brandao@fpf.br>
2022-09-12 15:31:40 -0400

    add gitignore

--- basic/admin.py
+++ basic/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from basic import models
+
+
 # Register your models here.
+@admin.register(models.Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'modified_at', 'active']
+    list_display_links = ['id', 'name', 'modified_at', 'active']
+    search_fields = ['name']
